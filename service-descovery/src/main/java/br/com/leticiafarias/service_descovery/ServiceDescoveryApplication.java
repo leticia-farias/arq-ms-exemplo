61ecc33dc43999ab9b75506c2ae9b429afda1ddf
leticia-farias <leticiafarias.engsoftware@gmail.com>
2026-06-04 15:50:46 -0300

    first commit big bang como diz a lanna dey redes

---
+++ service-descovery/src/main/java/br/com/leticiafarias/service_descovery/ServiceDescoveryApplication.java
@@ -0,0 +1,15 @@
+package br.com.leticiafarias.service_descovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class ServiceDescoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceDescoveryApplication.class, args);
+	}
+
+}
---
+++ service-descovery/src/test/java/br/com/leticiafarias/service_descovery/ServiceDescoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.leticiafarias.service_descovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDescoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
